acffa47610174f44b6627573ce1bb05f8fd11328
Elm2006 <143172014+Elm2006@users.noreply.github.com>
2023-10-13 13:58:03 +0200

    Add project files.

---
+++ uppgift 4.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace uppgift_4.__4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett tal");
+            string svar =Console.ReadLine();
+            int siffersumma = 0;
+            for (int i = 0; i < svar.Length; i++) 
+            {
+                string teckenString = svar[i].ToString();
+                int tal = int.Parse(teckenString);
+                siffersumma += tal;
+            }
+            Console.WriteLine("Siffersumman av talet " + svar + " = " + siffersumma);
+        }
+    }
+}
